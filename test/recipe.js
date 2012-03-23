120c139605ee3ab5f308fd3f961ec785b5d6c198
Jamie Hoover ⚔ <dont.tase@me.com>
2012-03-22 20:19:31 -0700

    Cleaner test output.

--- test/recipe.js
+++ test/recipe.js
@@ -28,24 +28,30 @@ describe('recipe', function () {
     console.log(yummly.name);
   });
 
-  it('should have ingredients', function () {
+  it('should have an ingredient', function () {
     expect(yummly.ingredientLines).to.be.an(Array);
-    console.log(yummly.ingredientLines);
+    expect(yummly.ingredientLines[0]).to.be.ok();
+    console.log(yummly.ingredientLines[0]);
   });
 
-  it('should have nutrition estimates', function () {
+  it('should have calories', function () {
     expect(yummly.nutritionEstimates).to.be.an(Array);
-    console.log(yummly.nutritionEstimates);
+    expect(yummly.nutritionEstimates[0].attribute).to.equal('ENERC_KCAL');
+    console.log(yummly.nutritionEstimates[0].unit.plural + ':', yummly.nutritionEstimates[0].value);
   });
 
   it('should have images', function () {
     expect(yummly.images).to.be.an(Array);
-    console.log(yummly.images);
   });
 
   it('should have a small image', function () {
     expect(yummly.images[0].hostedSmallUrl).to.be.ok();
     console.log(yummly.images[0].hostedSmallUrl);
+  });
+
+  it('should have a large image', function () {
+    expect(yummly.images[0].hostedLargeUrl).to.be.ok();
+    console.log(yummly.images[0].hostedLargeUrl);
   });
 
 });
